beac8ebd06419f3e73424342cddbe1cb0508e7a0
Igor Bulyshev <exeleroon@gmail.com>
2021-03-23 20:16:26 +0200

    23.03 options

--- app/js/class/modal/main.js
+++ app/js/class/modal/main.js
@@ -19,3 +19,39 @@ const modalWindow = new Modal({
 container.append(modalWindow.render());
 let popup = document.querySelector(".background-popup");
 let popupClass = document.querySelector(".popup");
+
+
+
+
+class regModal extends Tools {
+  constructor({elem, className}) {
+    super(elem);
+    this.className = className;
+  }
+  render() {
+    this.div = this.createElement();
+    this.div.className = this.className;
+    this.div.insertAdjacentHTML('afterbegin', '<div class=regLabel><form name=registration class=reg-form>');
+    return this.div
+  }
+}
+
+const modalReg = new regModal({
+  elem: 'div',
+  className: 'reg-form'
+});
+
+container.append(modalReg.render());
+let regForm = document.querySelector('.reg-form');
+let regLabel = document.querySelector('.regLabel');
+
+const activeReg = document.querySelector('.popup');
+
+
+activeReg.addEventListener('click', () => {
+  regForm.classList.add('form__active');
+});
+
+
+
+

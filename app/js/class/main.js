54decc1a4a38926c334b60932a48a776529afde3
Sviatoslav <sviatoslavsodel@gmail.com>
2021-03-25 16:10:04 +0200

    Update main.js

--- app/js/class/main.js
+++ app/js/class/main.js
@@ -22,18 +22,19 @@ let container = document.body;
 @@include("options/main.js");
 @@include("textarea/main.js");
 @@include("filter/main.js");
-
+let selectChooseDoctor;
 createButton.addEventListener("click", () => {
   createButton.style.display = "none";
   popup.classList.add("popup__active");
-  popupClass.append(ModalContent.render());
+  popupClass.append(formAuthorization.render())
+  let formClass = document.querySelector(".popup-form");
+  formClass.append(ModalContent.render());
   const createContentClass = document.querySelector(".popup-create-content");
   createContentClass.append(modalHeader.render());
   const createHeaderClass = document.querySelector(".popup-header");
   createHeaderClass.insertAdjacentHTML("afterbegin", "<h5 class=modal-title>Create Visit</h5>");
   createHeaderClass.append(btnClose.render());
   let closeModal = document.querySelector(".close-modal");
-  console.log(closeModal)
   closeModal.addEventListener('click', () => {
     popup.classList.remove('popup__active');
     popupClass.textContent = "";
@@ -44,7 +45,8 @@ createButton.addEventListener("click", () => {
   createBodyClass.append(chooseDoctors.render())
   createContentClass.append(modalFooter.render());
   const createFooterClass = document.querySelector(".popup-footer");
-  let selectChooseDoctor = document.querySelector(".choose-doctors");
+  createFooterClass.append(btnSubmit.render())
+   selectChooseDoctor = document.querySelector(".choose-doctors");
   selectChooseDoctor.append(
     optionDefault.render(),
     optionCardiologist.render(),

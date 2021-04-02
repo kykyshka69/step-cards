451293d275f009211ed47bf7a22ef1e2a70f6d42
Горбаченко Олег <espressobar88@gmail.com>
2021-04-02 18:11:06 +0300

    delete all

--- app/js/class/filter/main.js
+++
@@ -1,1 +0,0 @@
-let filterWindowClass = document.querySelector("modal-filter");
--- app/js/main.js
+++
@@ -1,5 +0,0 @@
-//тут подключаем файлы JS, никаких разделителей в конце строки не ставить!
-@@include("class/main.js");
-if (window.getSelection().rangeCount >= 1) {
-  var r = window.getSelection().getRangeAt(0);
-}

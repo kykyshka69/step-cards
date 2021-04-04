d37a16e8affd25b142f4403e30207e9ab1e9e7d2
Горбаченко Олег <espressobar88@gmail.com>
2021-04-04 03:07:31 +0300

    add create cards and buttons

--- app/js/class/request/getCard.js
+++ app/js/class/request/getCard.js
@@ -1,4 +1,4 @@
-function getCard() {
+function getCard(e) {
   fetch("https://ajax.test-danit.com/api/v2/cards", {
     method: "GET",
     headers: {
@@ -11,10 +11,41 @@ function getCard() {
     })
     .then((data) => {
       data.forEach((element) => {
-        const { doctor } = element;
-        document.body.innerHTML += `<ul>
-        <li>Doctor :${doctor} </li>
-    </ul>`;
+        const {
+          doctor,
+          purpose,
+          description,
+          urgency,
+          fullname,
+          pressure,
+          Indexbodymass,
+          cardiovascular,
+          age,
+          lastvisit,
+        } = element;
+        document.body.innerHTML += `<div class="card" style="width: 22rem;">
+        <div class="card-body">
+          <h5 class="card-title">${doctor}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${fullname}</h6>
+          <div class="content-info-patient">
+           <p class="card-text card-description">${description}</p>
+          <p class="card-text card-purpose">${purpose}</p>
+          <p class="card-text card-urgency">${urgency}</p>
+          <p class="card-text card-pressure">${pressure}</p>
+          <p class="card-text card-index-body-mass">${Indexbodymass}</p>
+          <p class="card-text card-cardio">${cardiovascular}</p>
+          <p class="card-text card-age">${age}</p>
+          <p class="card-text card-last-visit">${lastvisit}</p>
+          </div>
+          <div class="row buttons-cards justify-content-between">
+          <button  class="btn  btn-show-more btn-primary btn-sm col-5">Показать больше</button>
+          <button class="btn btn-edit  btn-primary btn-sm col-5">Редактировать</button>
+
+          </div>
+        </div>
+      </div>`;
       });
     });
 }
+
+getCard();

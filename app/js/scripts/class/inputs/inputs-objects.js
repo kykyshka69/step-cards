b7536435ca440b495501828335991bdb76e5db08
Горбаченко Олег <espressobar88@gmail.com>
2021-04-07 20:00:48 +0300

    add input

--- app/js/scripts/class/inputs/inputs-objects.js
+++ app/js/scripts/class/inputs/inputs-objects.js
@@ -1,42 +1,65 @@
 const inputLoginClass = new Input({
-    name: "login",
-    className: "input-login form-control",
-    type: "email",
-    placeholder: "Your email",
-    elem: "input",
+  name: "login",
+  className: "input-login form-control",
+  type: "email",
+  placeholder: "Your email",
+  elem: "input",
 });
 
-
-const inputTargetClass = new Input ({
-    name: "visit-target",
-    className: "visit__target form-control",
-    type: "text",
-    placeholder: "Цель визита",
-    elem: 'input',
+const inputTargetClass = new Input({
+  name: "visit-target",
+  className: "visit__target form-control",
+  type: "text",
+  placeholder: "Цель визита",
+  elem: "input",
 });
 
-const inputDescriptionClass = new Input ({
-    name: 'Description',
-    className: 'input_description form-control',
-    type: 'text',
-    placeholder: 'Краткое описание визита',
-    elem: 'input'
+const inputDescriptionClass = new Input({
+  name: "Description",
+  className: "input_description form-control",
+  type: "text",
+  placeholder: "Краткое описание визита",
+  elem: "input",
 });
 
-const inputFullNameClass = new Input ({
-    name: 'fullName',
-    className: 'input-fullName form-control',
-    type: 'text',
-    placeholder: 'ФИО',
-    elem: 'input'
+const inputFullNameClass = new Input({
+  name: "fullName",
+  className: "input-fullName form-control",
+  type: "text",
+  placeholder: "ФИО",
+  elem: "input",
 });
 
 const inputAgeClass = new Input({
-    name: "age",
-    className: "input-age form-control",
-    type: "text",
-    placeholder: "Your age",
-    elem: "input",
+  name: "age",
+  className: "input-age form-control",
+  type: "text",
+  placeholder: "Your age",
+  elem: "input",
+});
+
+const inputPressureClass = new Input({
+  name: "pressure",
+  className: "input-pressure form-control",
+  type: "text",
+  placeholder: "Обычное давление",
+  elem: "input",
+});
+
+const inputWeightClass = new Input({
+  name: "weight",
+  className: "input-weight form-control",
+  type: "text",
+  placeholder: "Индекс массы тела",
+  elem: "input",
+});
+
+const inputLastVisitClass = new Input({
+  name: "date",
+  className: "input-date form-control",
+  type: "text",
+  placeholder: "Дата последнего визита",
+  elem: "input",
 });
 
 const inputLoginElement = inputLoginClass.render();
@@ -44,3 +67,6 @@ const inputTargetElement = inputTargetClass.render();
 const inputDescriptionElement = inputDescriptionClass.render();
 const inputFullNameElement = inputFullNameClass.render();
 const inputAgeElement = inputAgeClass.render();
+const inputPressureElement = inputPressureClass.render();
+const inputWeightElement = inputWeightClass.render();
+const inputLastVisit = inputLastVisitClass.render();

b3d68a628f1a0e532c304663b2ad32b2ab6f52c7
Горбаченко Олег <espressobar88@gmail.com>
2021-04-08 20:29:09 +0300

    add all doctors posts

--- app/js/scripts/request/request-posts.js
+++ app/js/scripts/request/request-posts.js
@@ -1,7 +1,9 @@
 buttonPostElement.onclick = (e) => {
   e.preventDefault();
   document.querySelector(".background-popup").classList.remove("popup__active");
-
+  modalFooterElement.textContent = "";
+  modalBodyElement.innerHTML = "";
+  chooseDoctorsElement.value = "Выбор доктора";
   if (chooseDoctorsElement.value === "Dentist") {
     createPost({
       doctor: "Стоматолог",
@@ -11,7 +13,7 @@ buttonPostElement.onclick = (e) => {
       lastVisit: inputLastVisitElement.value,
       fullName: inputFullNameElement.value,
     });
-  }else if (chooseDoctorsElement.value=== "Cardiologist") {
+  } else if (chooseDoctorsElement.value === "Cardiologist") {
     createPost({
       doctor: "Кардиолог",
       purpose: inputTargetElement.value,
@@ -23,16 +25,16 @@ buttonPostElement.onclick = (e) => {
       cardioVascular: textAreaElement.value,
       age: inputAgeElement.value,
     });
-  // } else if (document.querySelector(".choose-doctors").value === "Терапевт") {
-  //   createPost({
-  //     doctor: "Терапевт",
-  //     purpose: document.querySelector(".visit__target").value,
-  //     description: document.querySelector(".input_description").value,
-  //     urgency: document.querySelector(".choose-urgency").value,
-  //     age: document.querySelector(".input-age").value,
-  //     fullname: document.querySelector(".input-fullName").value,
-  //   });
-  // }
+  } else if (chooseDoctorsElement.value === "Therapist") {
+    createPost({
+      doctor: "Терапевт",
+      purpose: inputTargetElement.value,
+      description: inputDescriptionElement.value,
+      urgency: chooseUrgencyElement.value,
+      age: inputAgeElement.value,
+      fullName: inputFullNameElement.value,
+    });
+  }
 };
 function createPost(body) {
   fetch("https://ajax.test-danit.com/api/v2/cards", {
